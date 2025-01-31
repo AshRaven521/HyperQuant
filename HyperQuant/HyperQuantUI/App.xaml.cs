8c0454b77f8dd8bbdb676beb4584f3c4c6493b6a
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-01-31 18:17:23 +0300

    add base for dependency injection

--- HyperQuant/HyperQuantUI/App.xaml.cs
+++ HyperQuant/HyperQuantUI/App.xaml.cs
@@ -1,5 +1,6 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using HyperQuantUI.ViewModel;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Windows;
 
 namespace HyperQuantUI
@@ -9,6 +10,39 @@ namespace HyperQuantUI
     /// </summary>
     public partial class App : Application
     {
+        public static IHost AppHost { get; private set; }
+        public App()
+        {
+            AppHost = Host.CreateDefaultBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddScoped<MainViewModel>();
+                    services.AddSingleton<MainWindow>();
+                })
+                .Build();
+        }
+
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await AppHost.StartAsync();
+
+
+            var startForm = AppHost.Services.GetRequiredService<MainWindow>();
+            startForm.DataContext = AppHost.Services.GetRequiredService<MainViewModel>();
+            startForm.Show();
+
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            var viewModel = AppHost.Services.GetRequiredService<MainViewModel>();
+            await AppHost.StopAsync();
+            AppHost.Dispose();
+
+            base.OnExit(e);
+        }
     }
 
 }

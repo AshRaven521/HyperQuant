ef4ee01bb159bdc240d156666933fb9f65d4ce09
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-02-01 21:20:14 +0300

    add connector models

---
+++ HyperQuant/HyperQuantConnector/Models/Trade.cs
@@ -0,0 +1,30 @@
+﻿namespace HyperQuantConnector.Models
+{
+    public class Trade
+    {
+        /* Пояснение! Оставляю только поля, которые приходят в ответе на запрос по URI: https://api-pub.bitfinex.com/v2/trades/{pair}/hist?limit={maxCount}&sort=-1. А именно 4 поля КОНКРЕТНО ДЛЯ ПАРЫ! */
+
+
+        /// <summary>
+        /// Id трейда
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Время трейда
+        /// </summary>
+        public DateTimeOffset Time { get; set; }
+        /// <summary>
+        /// Валютная пара
+        /// </summary>
+        public string Pair { get; set; }
+
+        /// <summary>
+        /// Цена трейда
+        /// </summary>
+        public decimal Price { get; set; }
+
+
+
+    }
+}

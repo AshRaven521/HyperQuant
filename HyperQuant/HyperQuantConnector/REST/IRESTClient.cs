e6892f7dacc710b070567b4a1c608ac0e71b8786
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-02-01 21:21:52 +0300

    add connector clients after parsing given interface (with only one realization))

---
+++ HyperQuant/HyperQuantConnector/REST/IRESTClient.cs
@@ -0,0 +1,10 @@
+﻿using HyperQuantConnector.Models;
+
+namespace HyperQuantConnector.REST
+{
+    public interface IRESTClient
+    {
+        Task<IEnumerable<Trade>> GetNewTradesAsync(string pair, int maxCount);
+        Task<IEnumerable<Candle>> GetCandleSeriesAsync(string pair, int periodInSec, DateTimeOffset? from, DateTimeOffset? to = null, long? count = 0);
+    }
+}
---
+++ HyperQuant/HyperQuantConnector/REST/RESTClient.cs
@@ -0,0 +1,34 @@
+﻿using HyperQuantConnector.Models;
+using RestSharp;
+
+namespace HyperQuantConnector.REST
+{
+    public class RESTClient : IRESTClient
+    {
+        public Task<IEnumerable<Candle>> GetCandleSeriesAsync(string pair, int periodInSec, DateTimeOffset? from, DateTimeOffset? to = null, long? count = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Trade>> GetNewTradesAsync(string pair, int maxCount)
+        {
+            var options = new RestClientOptions($"https://api-pub.bitfinex.com/v2/trades/{pair}/hist?limit={maxCount}&sort=-1");
+            var client = new RestClient(options);
+            var request = new RestRequest("");
+
+            request.AddHeader("accept", "application/json");
+
+            var response = await client.GetAsync(request);
+
+            if (response.IsSuccessful && !string.IsNullOrWhiteSpace(response.Content))
+            {
+                var trades = JSONParser.ParseTrades(response.Content).ToList();
+
+                return trades;
+            }
+            
+            return new List<Trade>();
+
+        }
+    }
+}

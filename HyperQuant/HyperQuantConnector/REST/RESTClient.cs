3d4d9a27b70059aa3b442bf13ab11fe1abe32d37
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-02-03 23:31:05 +0300

    add candles implementation

--- HyperQuant/HyperQuantConnector/REST/RESTClient.cs
+++ HyperQuant/HyperQuantConnector/REST/RESTClient.cs
@@ -1,13 +1,40 @@
 ﻿using HyperQuantConnector.Models;
 using RestSharp;
+using System.Text;
 
 namespace HyperQuantConnector.REST
 {
     public class RESTClient : IRESTClient
     {
-        public Task<IEnumerable<Candle>> GetCandleSeriesAsync(string pair, int periodInSec, DateTimeOffset? from, DateTimeOffset? to = null, long? count = 0)
+        public async Task<IEnumerable<Candle>> GetCandleSeriesAsync(string pair, int periodInSec, DateTimeOffset? from, DateTimeOffset? to = null, long? count = 0)
         {
-            throw new NotImplementedException();
+            string periodParam = CustomConverter.GetCandleQueryParamByPeriod(periodInSec);
+
+            StringBuilder uriBuilder = new StringBuilder();
+
+            if (to == null)
+            {
+                uriBuilder.AppendLine($"https://api-pub.bitfinex.com/v2/candles/trade%3A{periodParam}%3A{pair}/hist?sort=-1&start={from}&limit={count}");
+            }
+            else
+            {
+                uriBuilder.AppendLine($"https://api-pub.bitfinex.com/v2/candles/trade%3A{periodParam}%3A{pair}/hist?sort=-1&start={from}&end={to}&limit={count}");
+            }
+
+            var options = new RestClientOptions(uriBuilder.ToString());
+            var client = new RestClient(options);
+
+            var request = new RestRequest("");
+            request.AddHeader("accept", "application/json");
+            var response = await client.GetAsync(request);
+
+            if (response.IsSuccessful && !string.IsNullOrWhiteSpace(response.Content))
+            {
+                var candles = CustomConverter.ParseCandles(response.Content);
+                return candles;
+            }
+
+            return new List<Candle>();
         }
 
         public async Task<IEnumerable<Trade>> GetNewTradesAsync(string pair, int maxCount)
@@ -22,7 +49,7 @@ namespace HyperQuantConnector.REST
 
             if (response.IsSuccessful && !string.IsNullOrWhiteSpace(response.Content))
             {
-                var trades = JSONParser.ParseTrades(response.Content).ToList();
+                var trades = CustomConverter.ParseTrades(response.Content).ToList();
 
                 return trades;
             }

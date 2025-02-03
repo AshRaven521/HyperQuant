2f9e5c14a8766a9dbff9172e0e658ab76554a847
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-02-03 23:31:56 +0300

    add navigation between pages

---
+++ HyperQuant/HyperQuantUI/Services/INavigationService.cs
@@ -0,0 +1,10 @@
+﻿using HyperQuantUI.ViewModel;
+
+namespace HyperQuantUI.Services
+{
+    public interface INavigationService
+    {
+        BaseViewModel CurrentView { get; }
+        void NavigateTo<T>() where T : BaseViewModel;
+    }
+}
---
+++ HyperQuant/HyperQuantUI/Services/NavigationService.cs
@@ -0,0 +1,39 @@
+﻿using HyperQuantUI.ViewModel;
+
+namespace HyperQuantUI.Services
+{
+    public class NavigationService : BaseViewModel, INavigationService
+    {
+        private BaseViewModel currentView;
+        private readonly Func<Type, BaseViewModel> viewModelFactory;
+
+        public BaseViewModel CurrentView
+        {
+            get
+            {
+                return currentView;
+            }
+            private set
+            {
+                if (currentView == value)
+                {
+                    return;
+                }
+                currentView = value;
+                OnPropertyChanged(nameof(CurrentView));
+            }
+
+        }
+
+        public NavigationService(Func<Type, BaseViewModel> viewModelFactory)
+        {
+            this.viewModelFactory = viewModelFactory;
+        }
+
+        public void NavigateTo<TVIewModel>() where TVIewModel : BaseViewModel
+        {
+            BaseViewModel viewModel = viewModelFactory.Invoke(typeof(TVIewModel));
+            CurrentView = viewModel;
+        }
+    }
+}

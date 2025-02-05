e31939d0a58cfbf598e9d0877a5c24202e9e1820
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-02-05 18:38:24 +0300

    update response parsing for funding currencies

--- HyperQuant/HyperQuantConnector/CustomConverter.cs
+++ HyperQuant/HyperQuantConnector/CustomConverter.cs
@@ -1,4 +1,5 @@
 ﻿using HyperQuantConnector.Models;
+using System.Diagnostics;
 using System.Globalization;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -8,7 +9,7 @@ namespace HyperQuantConnector
     public class CustomConverter
     {
         /* Парсим вручную ответ с API Bitnefix при запросе данных на пару валют */
-        public static IEnumerable<Trade> ParseTrades(string json)
+        public static IEnumerable<Trade> ParseTrades(string json, bool isFundingCurrency = false)
         {
             var dataArray = json.Replace(']', ' ');
             var splittedDataArray = dataArray.Split('[');
@@ -23,15 +24,28 @@ namespace HyperQuantConnector
                 }
                 var splittedByComaArray = splittedData.Split(',');
 
+                IFormatProvider formatter = new NumberFormatInfo { NumberDecimalSeparator = "." };
 
-
-                Trade trade = new Trade
+                Trade trade = new Trade();
+                // Если рассматриваем пару валют (например, tBTCUSD). Иначе, если рассматриваем funding currency (например, fUSD), то поля объекта заполняем немного по-другому
+                if (!isFundingCurrency)
                 {
-                    Id = splittedByComaArray[0],
-                    Time = DateTimeOffset.FromUnixTimeMilliseconds(long.Parse(splittedByComaArray[1])),
-                    Pair = splittedByComaArray[2],
-                    Price = decimal.Parse(splittedByComaArray[3])
-                };
+                    trade.Id = splittedByComaArray[0];
+                    trade.Time = DateTimeOffset.FromUnixTimeMilliseconds(long.Parse(splittedByComaArray[1]));
+                    trade.Pair = splittedByComaArray[2];
+                    trade.Price = decimal.Parse(splittedByComaArray[3], formatter);
+                    trade.Rate = null;
+                    trade.Period = null;
+                }
+                else
+                {
+                    trade.Id = splittedByComaArray[0];
+                    trade.Time = DateTimeOffset.FromUnixTimeMilliseconds(long.Parse(splittedByComaArray[1]));
+                    trade.Pair = splittedByComaArray[2];
+                    trade.Price = null;
+                    trade.Rate = float.Parse(splittedByComaArray[3], formatter);
+                    trade.Period = int.Parse(splittedByComaArray[4]);
+                }
 
                 result.Add(trade);
 
@@ -61,10 +75,10 @@ namespace HyperQuantConnector
                 Candle candle = new Candle
                 {
                     OpenTime = DateTimeOffset.FromUnixTimeMilliseconds(long.Parse(splittedByComaArray[0])),
-                    Open = int.Parse(splittedByComaArray[1]),
-                    Close = int.Parse(splittedByComaArray[2]),
-                    High = int.Parse(splittedByComaArray[3]),
-                    Low = int.Parse(splittedByComaArray[4]),
+                    Open = double.Parse(splittedByComaArray[1], formatter),
+                    Close = double.Parse(splittedByComaArray[2], formatter),
+                    High = double.Parse(splittedByComaArray[3], formatter),
+                    Low = double.Parse(splittedByComaArray[4], formatter),
                     Volume = double.Parse(splittedByComaArray[5], formatter)
                 };
 

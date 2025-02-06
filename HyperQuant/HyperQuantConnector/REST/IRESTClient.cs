089f844b32058fb0549450e2b020c7c9fd005ac2
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-02-06 22:52:00 +0300

    update with getTickersAsync function

--- HyperQuant/HyperQuantConnector/REST/IRESTClient.cs
+++ HyperQuant/HyperQuantConnector/REST/IRESTClient.cs
@@ -6,5 +6,6 @@ namespace HyperQuantConnector.REST
     {
         Task<IEnumerable<Trade>> GetNewTradesAsync(string pair, int maxCount);
         Task<IEnumerable<Candle>> GetCandleSeriesAsync(string pair, int periodInSec, DateTimeOffset? from, DateTimeOffset? to = null, long? count = 0);
+        Task<IEnumerable<Ticker>> GetTickersAsync(string pair);
     }
 }

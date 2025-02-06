ba398e7eb26e868a95dfa3b263e8022e0298a99b
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-02-06 22:51:36 +0300

    add ticker model

---
+++ HyperQuant/HyperQuantConnector/Models/Ticker.cs
@@ -0,0 +1,20 @@
+﻿namespace HyperQuantConnector.Models
+{
+    public class Ticker
+    {
+        public double? FRR { get; set; }
+        public double BID { get; set; }
+        public int? BID_PERIOD { get; set; }
+        public double BID_SIZE { get; set; }
+        public double ASK { get; set; }
+        public int? ASK_PERIOD { get; set; }
+        public double ASK_SIZE { get; set; }
+        public double DAILY_CHANGE { get; set; }
+        public double DAILY_CHANGE_PERC { get; set; }
+        public double LAST_PRICE { get; set; }
+        public double VOLUME { get; set; }
+        public double HIGH { get; set; }
+        public double LOW { get; set; }
+        public double? FRR_AMOUNT_AVAILABLE { get; set; }
+    }
+}

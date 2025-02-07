ce85bb5f8a8b7af0bb449b30e327a92c798b7666
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-02-07 20:51:21 +0300

    add interface working with Coin Layer API

---
+++ HyperQuant/HyperQuantConnector/CoinLayer/ICoinLayerApi.cs
@@ -0,0 +1,11 @@
+﻿
+using HyperQuantConnector.Models;
+
+namespace HyperQuantConnector.CoinLayer
+{
+    public interface ICoinLayerApi
+    {
+        Task<IDictionary<string, decimal>> GetExchangeRates(string startCurrency, IEnumerable<string> neededCurrencies, string configFilePath);
+        IDictionary<string, decimal> CalculateCurrencyCount(IEnumerable<CryptoCurrency> currencies);
+    }
+}

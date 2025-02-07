333f5f149bcebaa4cb555cab8efb5b48f5d5f0d6
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-02-07 21:01:51 +0300

    update with json property name for deserialization

--- HyperQuant/HyperQuantConnector/CoinLayer/ApiKey.cs
+++ HyperQuant/HyperQuantConnector/CoinLayer/ApiKey.cs
@@ -1,7 +1,10 @@
-﻿namespace HyperQuantConnector.CoinLayer
+﻿using System.Text.Json.Serialization;
+
+namespace HyperQuantConnector.CoinLayer
 {
     public class ApiKey
     {
+        [JsonPropertyName("CoinLayerApi")]
         public string Key { get; set; }
     }
 }

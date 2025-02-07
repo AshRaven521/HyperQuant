745155e356a04526397bab27333c340e89ef006c
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-02-07 20:49:18 +0300

    add method for getting api key from config file

---
+++ HyperQuant/HyperQuantConnector/Heplers/ConfigurationHelper.cs
@@ -0,0 +1,22 @@
+﻿using HyperQuantConnector.CoinLayer;
+using System.Text.Json;
+
+namespace HyperQuantConnector.Heplers
+{
+    public static class ConfigurationHelper
+    {
+        public static string GetApiKey(string filePath)
+        {
+            string json = File.ReadAllText(filePath);
+            var apiKey = JsonSerializer.Deserialize<ApiKey>(json);
+
+            if (string.IsNullOrEmpty(apiKey.Key))
+            {
+                return string.Empty;
+            }
+            return apiKey.Key;
+
+        }
+
+    }
+}

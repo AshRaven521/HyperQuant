75bbbc55af9950c7ec6572a6e0f052ea387b36e7
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-02-07 20:49:50 +0300

    add model for final crypto entity

---
+++ HyperQuant/HyperQuantConnector/Models/Crypto/CryptoBriefCase.cs
@@ -0,0 +1,29 @@
+﻿namespace HyperQuantConnector.Models.Crypto
+{
+    /// <summary>
+    /// Портфель с кол-вом криптовалюты
+    /// </summary>
+    public class CryptoBriefCase
+    {
+
+        public decimal USDTAmount { get; }
+        public decimal BTCAmount { get; }
+        public decimal XRPAmount { get; }
+        public decimal XMRAmount { get; }
+        public decimal DASHAmount { get; }
+
+        public CryptoBriefCase(decimal uSDT, decimal bTC, decimal xRP, decimal xMR, decimal dASH)
+        {
+            USDTAmount = uSDT;
+            BTCAmount = bTC;
+            XRPAmount = xRP;
+            XMRAmount = xMR;
+            DASHAmount = dASH;
+        }
+
+        public CryptoBriefCase()
+        {
+
+        }
+    }
+}

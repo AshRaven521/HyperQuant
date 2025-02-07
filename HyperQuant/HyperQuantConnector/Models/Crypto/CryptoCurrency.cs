fcba28e938ab8da045d59c2db91e4a52a1924f1a
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-02-07 20:50:04 +0300

    add model for crypto currency

---
+++ HyperQuant/HyperQuantConnector/Models/Crypto/CryptoCurrency.cs
@@ -0,0 +1,9 @@
+﻿namespace HyperQuantConnector.Models
+{
+    public class CryptoCurrency
+    {
+        public CryptoType Type { get; set; }
+        public int Count { get; set; }
+        public IDictionary<string, decimal>? ExchangeRates { get; set; }
+    }
+}

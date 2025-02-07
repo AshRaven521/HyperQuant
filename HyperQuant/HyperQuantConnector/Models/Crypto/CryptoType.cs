9a4b26bcfe98b879bbaca1a7fafda163eebd8750
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-02-07 20:50:13 +0300

    add crypto type enum

---
+++ HyperQuant/HyperQuantConnector/Models/Crypto/CryptoType.cs
@@ -0,0 +1,11 @@
+﻿namespace HyperQuantConnector.Models.Crypto
+{
+    public enum CryptoType
+    {
+        USDT = 1,
+        BTC = 2,
+        XRP = 3,
+        XMR = 4,
+        DASH = 5
+    }
+}

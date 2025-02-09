724db9841fa6f206b88e51ad46889f9d2ffe035b
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-02-10 00:14:41 +0300

    update function return type for asynchronius

--- HyperQuant/HyperQuantConnector/WebSocket/IWebSocketClient.cs
+++ HyperQuant/HyperQuantConnector/WebSocket/IWebSocketClient.cs
@@ -6,11 +6,12 @@ namespace HyperQuantConnector.WebSocket
     {
         event Action<Trade> NewBuyTrade;
         event Action<Trade> NewSellTrade;
-        void SubscribeTrades(string pair, int maxCount = 100);
+        IEnumerable<Trade> ParsedTrades { get; }
+        Task SubscribeTrades(string pair, int maxCount = 100);
         void UnsubscribeTrades(string pair);
 
         event Action<Candle> CandleSeriesProcessing;
-        void SubscribeCandles(string pair, int periodInSec, DateTimeOffset? from = null, DateTimeOffset? to = null, long? count = 0);
+        Task SubscribeCandles(string pair, int periodInSec, DateTimeOffset? from = null, DateTimeOffset? to = null, long? count = 0);
         void UnsubscribeCandles(string pair);
     }
 }

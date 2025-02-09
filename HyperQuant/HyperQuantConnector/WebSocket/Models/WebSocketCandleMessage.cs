4c2343fef531e4ba5b7ac0df9a5a1d4920088e27
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-02-10 00:15:08 +0300

    add models for web socket messages

---
+++ HyperQuant/HyperQuantConnector/WebSocket/Models/WebSocketCandleMessage.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HyperQuantConnector.WebSocket.Models
+{
+    public class WebSocketCandleMessage
+    {
+        [JsonPropertyName("event")]
+        public string EventType { get; set; }
+
+        [JsonPropertyName("channel")]
+        public string Channel { get; set; }
+
+        [JsonPropertyName("key")]
+        public string Key { get; set; }
+    }
+}
---
+++ HyperQuant/HyperQuantConnector/WebSocket/Models/WebSocketTradeMessage.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HyperQuantConnector.WebSocket.Models
+{
+    public class WebSocketTradeMessage
+    {
+
+        [JsonPropertyName("event")]
+        public string EventType { get; set; }
+
+        [JsonPropertyName("channel")]
+        public string Channel { get; set; }
+
+        [JsonPropertyName("symbol")]
+        public string Symbol { get; set; }
+
+
+    }
+}
